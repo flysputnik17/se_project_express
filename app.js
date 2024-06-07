0010306bb66ffc3661d7b88a25701934ee96d318
KODKOD177 <pavel2012155@gmail.com>
2024-06-07 14:19:19 +0300

    login done errors and req

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 const { errors } = require("celebrate");
 
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 const app = express();
 const { PORT = 3001 } = process.env;
 
@@ -19,7 +21,9 @@ mongoose
 
 app.use(express.json());
 app.use(cors());
+app.use(requestLogger);
 app.use("/", mainRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
