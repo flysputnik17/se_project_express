8508008b1cda753a2ebf79ae387b12a79dc1f515
KODKOD177 <pavel2012155@gmail.com>
2024-06-14 13:53:12 +0300

    minor fix

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
 const { getCurrentUser, upDateCurrentUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
-const {
-  validateUserUpdate,
-  validateAuthentication,
-} = require("../middlewares/validation");
+const { validateUserUpdate } = require("../middlewares/validation");
 
 router.use(auth);
 
-router.get("/me", validateAuthentication, getCurrentUser);
+router.get("/me", getCurrentUser);
 router.patch("/me", validateUserUpdate, upDateCurrentUser);
 
 module.exports = router;
