ce3c9e2af6ff5d12f1086ac31ef50ad1b106e4c8
KODKOD177 <pavel2012155@gmail.com>
2024-09-09 16:54:38 +0300

    Limiter update

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,8 +1,9 @@
-const { rateLimit } = require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 
-module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: "Too many requests, please try again later",
 });
+
+module.exports = limiter;
